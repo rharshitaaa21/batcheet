558410efd35045a7833fdeaec5ef430712ddc858
Harshita Raghav <raghav.harshita21@gmail.com>
2022-07-12 02:19:35 +0530

    Updated Changes
    Changed the colour

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          primarySwatch: Colors.pink,
+          primarySwatch: Colors.purple,
           backgroundColor: Colors.pink,
           accentColor: Colors.deepPurple,
           accentColorBrightness: Brightness.dark,
